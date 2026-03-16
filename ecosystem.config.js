c5627b2e55478206c9090cacbde4f9ca4d00c033
himanthajaga <himanthagamachchige@gmail.com>
2026-03-16 23:30:19 +0530

    Refactor ecosystem.config.js to simplify arguments and standardize log file naming

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,38 +3,20 @@ module.exports = {
     {
       name: "Config-Server",
       script: "java",
-      args: ["-jar", "./Config-Server/target/Config-Server-1.0.0.jar"],
-      cwd: ".",
-      autorestart: true,
-      restart_delay: 8000,
-      max_restarts: 10,
-      out_file: "./logs/config-server-out.log",
-      error_file: "./logs/config-server-error.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss"
+      args: "-jar ./Config-Server/target/Config-Server-1.0.0.jar",
+      log_file: "./logs/config-server-out.log",
     },
     {
       name: "platform-registry",
       script: "java",
-      args: ["-jar", "./platform-registry/target/Service-Registry-1.0.0.jar"],
-      cwd: ".",
-      autorestart: true,
-      restart_delay: 8000,
-      max_restarts: 10,
-      out_file: "./logs/platform-registry-out.log",
-      error_file: "./logs/platform-registry-error.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss"
+      args: "-jar ./platform-registry/target/Service-Registry-1.0.0.jar",
+      log_file: "./logs/platform-registry-out.log",
     },
     {
-      name: "Api-Gateway",
+      name: "Api-gateway",
       script: "java",
-      args: ["-jar", "./Api-gateway/target/Api-Gateway-1.0.0.jar"],
-      cwd: ".",
-      autorestart: true,
-      restart_delay: 8000,
-      max_restarts: 10,
-      out_file: "./logs/api-gateway-out.log",
-      error_file: "./logs/api-gateway-error.log",
-      log_date_format: "YYYY-MM-DD HH:mm:ss"
+      args: "-jar ./Api-gateway/target/Api-Gateway-1.0.0.jar",
+      log_file: "./logs/api-gateway-out.log",
     }
   ]
 };
